c102a7643a6acf258ea1fb08afc81a92f59b536f
Juan Manuel Berros <juanmaberros@gmail.com>
2015-06-06 13:30:47 -0300

    mail Calvin & Hobbes script

---
+++ mail_calvin.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+require 'nokogiri'
+require 'pony'
+require 'colorize'
+
+url = "http://www.gocomics.com/calvinandhobbes"
+doc = Nokogiri::HTML(open(url))
+img_url = doc.css("img[alt='Calvin and Hobbes']").first['src']
+
+target_dir = "#{Dir.home}/Dropbox/calvin_strips"
+filename = Date.today.strftime('%A_%F').downcase
+fullpath = "#{target_dir}/#{filename}.gif"
+
+FileUtils.mkdir(target_dir) unless File.directory? target_dir
+`wget -O #{fullpath} #{img_url}`
+
+Pony.mail(
+  subject: "Calvin Strip ~ #{Date.today.strftime('%A %F')} ",
+  from: "'JuanBOT' <juanbot@beleriand>",
+  to: ["'The Juanma' <juanmaberros@gmail.com>", "'Peluca' <arami035@gmail.com>"],
+  html_body: "<img src='#{img_url}' style='width: 80%;'>",
+  via: :smtp,
+  via_options: {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'juanmaberros',
+    password: "ngoinbcasuctiudk".reverse,
+    authentication: :plain,
+    domain: "localhost.localdomain"
+  }
+)
